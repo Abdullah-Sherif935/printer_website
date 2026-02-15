99f93aa959b560bd9eabdf0f3d67626ddd79d89b
Abdullah Sherif <eng.abdullah.sherif@gmail.com>
2026-02-15 02:50:56 +0200

    Use webpack for build to fix canvas issue

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,6 @@ const nextConfig: NextConfig = {
   typescript: {
     ignoreBuildErrors: true,
   },
-  turbopack: {},
   webpack: (config: any) => {
     config.resolve.alias.canvas = false;
     return config;
